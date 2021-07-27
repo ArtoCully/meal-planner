2e52c44c0a73704ace92602ccee2ec4839e74681
Arturo Cullinane <artocully@users.noreply.github.com>
2021-07-27 16:10:44 +0100

    Add react-cookie

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,14 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <CookiesProvider>
+        <App />
+      </CookiesProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
