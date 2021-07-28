d5475b3127daa7e204686a4d9f5b89bf01ec7c36
Arturo Cullinane <artocully@users.noreply.github.com>
2021-07-28 17:03:40 +0100

    Add initial login functionality

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,22 +1,52 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { login } from '../../services/authenticate';
 import DividerWithText from '../Divider/DividerWithText';
+import { ILogin } from '../../models/user';
 import './Login.css';
 
 export default function Login() {
+  const history = useHistory();
+  const formObject: ILogin = {
+    username: '',
+    password: '',
+  }
+
+  const [formState, setFormState] = React.useState(formObject);
+
+  const handleSetInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    const name = event.target.name;
+    const value = event.target.value;
+    const form = {
+      ...formState,
+      [name]: value,
+    }
+    setFormState(form);
+  }
+
+  const handleLogin = async (event: React.SyntheticEvent) => {
+    event.preventDefault();
+
+    const response = await login(formState);
+    if (response && response.status === 200) {
+      console.log('response', response);
+      history.push('/');
+    }
+  };
 
   return (
     <section className="App-section App-login">
       <h2>Login</h2>
       <form className="App-login__form">
         <div className="App-form-group">
-          <input type="text" id="App-login__form-username" name="username" placeholder="Enter your username" />
+          <input type="text" onChange={handleSetInput} id="App-login__form-username" name="username" placeholder="Enter your username" />
         </div>
         <div className="App-form-group">
-          <input type="password" id="App-login__form-password" name="password" placeholder="Enter your password" />
+          <input type="password" onChange={handleSetInput} id="App-login__form-password" name="password" placeholder="Enter your password" />
         </div>
         <div className="App-form-group">
-          <button className="App-btn App-btn--primary App-btn__login">Login</button>
+          <button className="App-btn App-btn--primary App-btn__login" onClick={handleLogin}>Login</button>
         </div>
         <DividerWithText text="OR" lineColour="#333" textColour="#333" />
         <div className="App-form-group">
--- src/services/authenticate.ts
+++ src/services/authenticate.ts
@@ -1,7 +1,9 @@
-import Cookie from 'js-cookie';
+import Cookies from 'js-cookie';
+import { authenticate } from './api';
+import { ILogin } from '../models/user';
 
 export const getCurrentUser = () => {
-  const user = Cookie.get('app_user') as string;
+  const user = Cookies.get('app_user') as string;
 
   if (user) {
     const userObject = JSON.parse(user);
@@ -11,11 +13,33 @@ export const getCurrentUser = () => {
   return false;
 }
 
+export const login = async ({ username, password }: ILogin) => {
+  try {
+    const authenticateResponse = await authenticate({
+      username,
+      password,
+    }).catch((err) => { throw err });
+
+    console.log('authenticateResponse', authenticateResponse);
+
+    if (authenticateResponse && authenticateResponse.status === 200) {
+      const { token, ...userData } = authenticateResponse.data;
+      await Cookies.set('app_tok', token, { expires: 7 })
+      await Cookies.set('app_user', JSON.stringify(userData), { expires: 7 })
+
+      return authenticateResponse;
+    }
+  } catch (e) {
+    console.log('Login error', e);
+    return e.response;
+  }
+}
+
 export const logout = () => {
-  Cookie.remove('app_user');
-  Cookie.remove('app_tok');
-  const app_user = Cookie.get('app_user');
-  const app_tok = Cookie.get('app_tok');
+  Cookies.remove('app_user');
+  Cookies.remove('app_tok');
+  const app_user = Cookies.get('app_user');
+  const app_tok = Cookies.get('app_tok');
 
   if (!app_user && !app_tok) {
     return true;
